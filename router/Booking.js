915592968b898fc6ff77e6d7f07f7c02926fbdf2
Your Name <you@example.com>
2024-03-03 17:21:24 +0700

    .

---
+++ router/Booking.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const axios = require('axios')
+var bodyParser = require('body-parser')
+const path = require('path');
+const { env } = require('process');
+const app = express();
+
+const base_url = "http://localhost:3000"
+
+app.set("views" , path.join(__dirname , "/public/views"))
+app.set("view engine" , "ejs")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
+
+app.use(express.static(__dirname + "/public"))
+//-------------------------------------Booking---------------------------------------
+app.get("/Booking", async (req, res) => {
+  try {
+    const response = await axios.get(base_url + "/Booking" );
+    console.log(response.data);
+      res.render("Booking/BookingAll", { Booking: response.data });
+  } catch(err) {
+      res.status(500).send(err);
+  }
+});
+
+app.get("/Booking/:id", async (req, res) => {
+  try {
+      const response = await axios.get(base_url + "/Booking/" + req.params.id);
+      res.render("Booking/Booking", { Booking: response.data });
+  } catch(err) {
+      res.status(500).send(err);
+  }
+});
+
+app.get("/Bookings/create", async (req, res) => {
+  try {
+  res.render("Booking/create");
+} catch(err) {
+  res.status(500).send(err)
+}   
+});
+
+app.post('/Bookings/create', async (req, res) => {
+  try {
+      const data = {
+        Booking_id: req.body.Booking_id,
+        User_id: req.body.User_id,
+        Type_id: req.body.Type_id,
+        Room: req.body.Room
+      };
+      await axios.post(base_url + "/Booking/", data); 
+      res.redirect("/Booking/");
+  } catch (error) {
+      res.status(500).send(error);
+  }
+});
+
+app.get("/Booking/update/:id", async (req, res) => {
+  try {         
+      const response = await axios.get(base_url + "/Booking/" + req.params.id);
+      res.render("Booking/update", { Booking: response.data });
+  } catch(err) {
+      res.status(500).send(err);
+  }
+});
+
+app.post("/Booking/update/:id", async (req, res) => {
+  try {
+      const data = {
+        Booking_id: req.body.Booking_id,
+        User_id: req.body.User_id,
+        Type_id: req.body.Type_id,
+        Room: req.body.Room
+      };
+      await axios.put(base_url + '/Booking/' + req.params.id, data); 
+      res.redirect('/Booking/');
+  } catch (error) {
+      res.status(500).send(error);
+  }
+});
+
+app.get("/Booking/delete/:id", async (req, res) => {
+  try {
+      await axios.delete(base_url + "/Booking/" + req.params.id);
+      res.redirect("/Booking");
+  } catch(err) {
+      res.status(500).send(err);
+  }
+});
